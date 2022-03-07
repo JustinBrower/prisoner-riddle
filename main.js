8868100ed0960bda9fa2172e2349cfd34c05ae4f
Justin Brower <justin.lee.brower@gmail.com>
2022-03-07 10:53:55 -0700

    comitt

--- main.js
+++ main.js
@@ -20,32 +20,33 @@ let switchTwo = Math.random() < 0.5
 let totalCount = 0
 let mainCount = 0
 let prisonerCount = 0
+let result = ""
 
 function riddle() {
-    console.log("Here we go!");
+    console.log("Riddle Starting...");
     for (let i = Math.round(Math.random() * 12); i <= prisoners.length; i = 1 * (Math.round(Math.random() * 12))) {
+        if (mainCount >= 26) {
+            return console.log("Please Reload...")
+        }
+        draw()
         prisonerCount++
         console.log("working...");
-        console.log("index ", i);
-        console.log("prisoner count ", prisonerCount);
-        console.log("main count ", mainCount);
-        console.log(switchOne);
-        console.log(switchTwo);
-        console.log("First", prisoners[i].first);
+        console.log("index", i);
+        console.log("prisoner count", prisonerCount);
+        console.log("main count", mainCount);
+        console.log("switch one", switchOne);
+        console.log("switch two", switchTwo);
+        console.log("first", prisoners[i].first);
         if (i == 0) {
             if (prisoners[i].first <= 1) {
                 prisoners[i].first++
                 mainCount++
             }
             if (switchOne == true) {
-                if (switchTwo == true) {
-                    switchTwo = false
-                } else {
-                    switchTwo = true
-                }
+                switchTwo = !switchTwo
             }
             else {
-                switchOne = true
+                switchOne = !switchOne
                 mainCount++
                 if (mainCount == 26) {
                     console.log('I raise my hand!')
@@ -54,35 +55,61 @@ function riddle() {
                         totalCount += prisoners[i].first
                     }
                     if (totalCount >= 25) {
-                        console.log("You Win!!!!");
+                        result = "You Win!!!!"
+                        console.log(result);
+                        draw()
                     } else {
-                        console.log("You Die.");
-                    } return
+                        result = "You Die."
+                        console.log(result);
+                        draw()
+                    }
+                    return
                 }
             }
         }
         else {
             if (prisoners[i].first <= 1) {
                 if (switchOne == true) {
-                    switchOne = false
+                    switchOne = !switchOne
                     prisoners[i].first++
                 }
                 else {
-                    if (switchTwo == true) {
-                        switchTwo = false
-                    } else {
-                        switchTwo = true
-                    }
+                    switchTwo = !switchTwo
                 }
-
             }
             else {
-                if (switchTwo == true) {
-                    switchTwo = false
-                } else {
-                    switchTwo = true
-                }
+                switchTwo = !switchTwo
             }
         }
     }
 }
+
+
+
+function draw() {
+    let template = `<div>Prisoners Sent: ${prisonerCount}</div>
+                    <div>Leader's Count: ${mainCount}</div>
+                    <div>First Time Enters: ${totalCount}</div>
+                    <div>Results: ${result}</div>`
+    let button = '<div><button onclick="riddle()">Riddle</button></div> <div><button onclick="reset()">Reset</button></div>'
+    document.getElementById('riddle').innerHTML = template
+    document.getElementById('button').innerHTML = button
+}
+function reset() {
+    for (let i = 0; i < prisoners.length; i++) {
+        prisoners[i].first = 0
+    }
+    switchOne = Math.random() < 0.5
+    switchTwo = Math.random() < 0.5
+
+    totalCount = 0
+    mainCount = 0
+    prisonerCount = 0
+    result = "Riddle Reset"
+    draw()
+    return
+}
+function check() {
+    console.log(totalCount, mainCount, prisonerCount);
+}
+draw()
